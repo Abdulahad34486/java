d2bf8daceaa0ce0f847d495778dcd27f26a82c71
Abdulahad34486 <maqboolahmed221968@gmail.com>
2024-10-01 11:17:11 +0100

    pushing code

---
+++ Totalprice.java
@@ -0,0 +1,16 @@
+public class Totalprice{
+
+
+         public static void main(String[] args){
+               
+             String produtsName = "Mobile";
+             int price=10000;
+             int numberofproducts =10;
+             int totalprice=price*numberofproducts;
+             System.out.println("produts name:"+produtsName);
+             System.out.println("total price is:"+totalprice);
+             }
+
+}            
+
+
---
+++ project_Name_solar_tracking_system.java
@@ -0,0 +1,10 @@
+public class project_Name_Solar_Tracking_System{
+
+              public static void main(String args[]) {
+
+
+                  System.out.println("project Name Solar Tracking System");
+     
+           }
+
+}
